37810bc78fbfd64a7c027180152f5e4fc87c8bf5
Rodolfo Henrique <rodolfo.coding@gmail.com>
2023-01-27 00:08:41 -0300

    chore: initial setup

--- maing.go
+++ maing.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/rodolfocoding/api-go-rest/models"
 	"github.com/rodolfocoding/api-go-rest/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Ininciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/rodolfocoding/api-go-rest/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonaldiade).Methods("Get")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
